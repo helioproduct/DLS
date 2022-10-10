03bcd3b2df5d2a830f5f95e1095ceab0227e75e8
helio <helioproduct.sync@gmail.com>
2022-10-10 23:11:23 +0300

    sklearn seminar sources added

--- 02_Numpy/5.py
+++ 02_Numpy/5.py
@@ -2,23 +2,24 @@ import numpy as np
 
 # Run-lenght encoding
 
-# encode(np.array([1, 2, 2, 3, 3, 1, 1, 5, 5, 2, 3, 3])) = (np.array([1, 2, 3, 1, 5, 2, 3]), np.array([1, 2, 2, 2, 2, 1, 2]))
+def encode(data):
+    elements = []
+    repeat = []
 
-# a = np.array([1, 2, 2, 3, 3, 1, 1, 5, 5, 2, 3, 3])
+    if len(data > 0):
+        elements.append(data[0])
+        repeat.append(1)
+    
+    for i in range(1, len(data)):
+        if data[i] == data[i - 1]:
+            repeat[-1] += 1
+        else:
+            elements.append(data[i])
+            repeat.append(1)
 
-a = np.array([1, 2, 3, 1, 5, 2, 3])
-b = np.array([1, 2, 2, 2, 2, 1, 2])
-
-
-
-def encode(a):
-    elements = np.array()
-    repeat = np.arary()
-
-    for i in range(len(a)):
-
-
-        
-
-
+    elements = np.array(elements)
+    repeat = np.array(repeat)
     return (elements, repeat)
+
+
+# print(*encode(data))
